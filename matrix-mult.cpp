130819a6ffb1d0a51ff82ddd0717eca9b1f7f5ec
Felipe Cortez <felipecortezfi@gmail.com>
2017-09-28 10:34:53 -0300

    Commit inicial

---
+++ matrix-mult.cpp
@@ -0,0 +1,16 @@
+// Matrix multiplication
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc != 3) {
+        cout << "Número de argumentos inválido" << endl;
+        cout << "Formato: " << endl;
+        cout << argv[0] << " x x" << endl;
+        return 0;
+    }
+
+    return 0;
+}
